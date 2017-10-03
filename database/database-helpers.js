8901d61bd0f12049827ddd57f8d329d4743ef5da
Ephraim Glick <ephraim.glick@gmail.com>
2017-10-03 09:28:30 -0700

    Add database-helpers file

---
+++ database/database-helpers.js
@@ -0,0 +1,5 @@
+const initGame = function() {
+  // Insert new entry into games table
+  // If we're generating the game ID here, just return it
+  // If auto-numbering games in db, query to get ID then return it
+};
