4df6066103e75f8db20f997085a3d5cb88c115b3
Ephraim Glick <ephraim.glick@gmail.com>
2017-10-03 10:00:32 -0700

    Add database-helpers file

--- server/index.js
+++ server/index.js
@@ -7,4 +7,40 @@ app.get('/', (req, res) => {
   res.send('index.html');
 });
 
+app.post('/login', (req, res) => {
+ // log user in
+});
+
+app.post('/games', (req, res) => {
+ // add to games table
+});
+
+app.get('/games/:gameId', (req, res) => {
+ // get from games table
+});
+
+app.post('/games/:gameId', (req, res) => {
+ // add to games table
+});
+
+app.post('/users', (req, res) => {
+ // add to users table
+});
+
+app.get('/users/:userId', (req, res) => {
+ // get info from users table
+});
+
+app.post('/users/:userId', (req, res) => {
+ // add to users table
+});
+
+app.get('/scores', (req, res) => {
+ // get from scores table
+});
+
+app.post('/scores', (req, res) => {
+ // add to scores table
+});
+
 app.listen(3000, () => console.log(`Express listening on port 3000`));
