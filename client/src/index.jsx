02d9181097d5796452c87d6951877311650948ef
Benjamin Stephens <17017460+speckospock@users.noreply.github.com>
2017-10-04 08:51:37 -0700

    No longer mutates state!!! WOOOO

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store.js';
-import { chats } from '../reducers/chatReducer.js';
+import { chats } from './reducers/chatReducer.js';
 import Board from './components/Board.jsx';
 
 class App extends React.Component {
