a936d2be40180c4cf78a68247b498fa75b1aae46
Benjamin Stephens <17017460+speckospock@users.noreply.github.com>
2017-10-04 20:52:10 -0700

    Comments actions

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,6 +2,9 @@ import { combineReducers } from 'redux';
 import boardReducer from './boardReducer.js';
 import chatReducer from './chatReducer.js';
 
+/**
+ * Takes in all reducers and exports them as one
+ */
 const reducers = combineReducers({
   chats: chatReducer,
   board: boardReducer,
