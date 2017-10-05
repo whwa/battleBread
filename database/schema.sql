60bb9c2a29595814ee5cdf4d72cb57a245d9401c
Devon Yu <devonyu415@gmail.com>
2017-10-04 21:41:21 -0700

    updated helper functions with template literals, removed last move from schema, added a streak column.  Need to see how we can use arrays for the state of the boards

--- database/schema.sql
+++ database/schema.sql
@@ -11,6 +11,7 @@ CREATE TABLE users (
   wins int NOT NULL DEFAULT 0,
   losses int NOT NULL DEFAULT 0,
   playerLevel int NOT NULL DEFAULT 1,
+  streak varchar(100) DEFAULT '',
   avatarUrl varchar(100) DEFAULT 'http://oi40.tinypic.com/i5sy1u.jpg',
   PRIMARY KEY (id)
 );
@@ -25,34 +26,33 @@ CREATE TABLE games (
   player2Placement varchar(100),
   player2Hits varchar(100),
   player2Misses varchar(100),
-  lastMove varchar(50) DEFAULT '[]',
-  result varchar(20) DEFAULT 'In progress',
+  result ENUM('In progress', 'Complete'),
   PRIMARY KEY (id),
   FOREIGN KEY (player1ID) REFERENCES users(id),
   FOREIGN KEY (player2ID) REFERENCES users(id)
 );
 
-INSERT into users (username, password, wins, losses, playerLevel, avatarUrl)
-Values ('boxingBakerBen', 'password1', 6, 4, 2, 'http://oi46.tinypic.com/4jl55k.jpg');
+INSERT into users (username, password, wins, losses, playerLevel, streak, avatarUrl)
+Values ('boxingBakerBen', 'password1', 6, 4, 2, 'WLWWLLWWWL', 'http://oi46.tinypic.com/4jl55k.jpg');
 
-INSERT into users (username, password, wins, losses, playerLevel, avatarUrl)
-Values ('doughDudeDevon', 'password2', 3, 7, 1, 'http://oi48.tinypic.com/2n9z89k.jpg');
+INSERT into users (username, password, wins, losses, playerLevel, streak, avatarUrl)
+Values ('doughDudeDevon', 'password2', 3, 7, 1, 'WLWWLLLLLL', 'http://oi48.tinypic.com/2n9z89k.jpg');
 
-INSERT into users (username, password, wins, losses, playerLevel, avatarUrl)
-Values ('empanadaEaterEphraim', 'password3', 5, 5, 2, 'http://oi50.tinypic.com/241jksn.jpg');
+INSERT into users (username, password, wins, losses, playerLevel, streak,avatarUrl)
+Values ('empanadaEaterEphraim', 'password3', 5, 5, 2, 'WWLWWLWLL', 'http://oi50.tinypic.com/241jksn.jpg');
 
-INSERT into users (username, password, wins, losses, playerLevel, avatarUrl)
-Values ('sniperSouffleSteph', 'password4', 8, 2, 3, 'http://oi43.tinypic.com/5twb9d.jpg');
+INSERT into users (username, password, wins, losses, playerLevel, streak, avatarUrl)
+Values ('sniperSouffleSteph', 'password4', 8, 2, 3, 'LWWLWWWWWW', 'http://oi43.tinypic.com/5twb9d.jpg');
 
 
-
-INSERT into games (player1ID, player1Placement, player1Hits, player1Misses, player2ID, 
-player2Placement, player2Hits, player2Misses, lastMove, result)
-Values (1, '[]', '[]', '[]', 2, '[]', '[]', '[]', null, null);
 
 INSERT into games (player1ID, player1Placement, player1Hits, player1Misses, player2ID, 
-player2Placement, player2Hits, player2Misses, lastMove, result)
-Values (3, '[]', '[]', '[]', 4, '[]', '[]', '[]', null, null);
+player2Placement, player2Hits, player2Misses, result)
+Values (1, '[]', '[]', '[]', 2, '[]', '[]', '[]', null);
+
+INSERT into games (player1ID, player1Placement, player1Hits, player1Misses, player2ID, 
+player2Placement, player2Hits, player2Misses, result)
+Values (3, '[]', '[]', '[]', 4, '[]', '[]', '[]', null);
 
 
 
