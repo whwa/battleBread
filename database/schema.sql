4727f46bfc935ac2a73027c196e9c56460c6abe2
Stephanie Wong <swongwrites@gmail.com>
2017-10-09 14:09:06 -0700

    Add chatWords

--- database/schema.sql
+++ database/schema.sql
@@ -29,24 +29,31 @@ CREATE TABLE games (
   allMoves varchar(400) DEFAULT '{"p1guess":[],"p2guess":[]}',
   chats varchar(10000),
   result ENUM('In progress', 'Complete'),
+  phrases varchar(200) DEFAULT '{
+      1: ["you", "and", "are", "toast", "batard"],
+      2: ["you", "and", "are", "toast", "batard", "cookies"],
+      3: ["you", "and", "are", "toast", "batard", "cookies", "Baguette"],
+      4: ["you", "and", "are", "toast", "batard", "cookies", "baguette", "I"],
+      4: ["you", "and", "are", "toast", "batard", "cookies", "baguette", "I", "bread"],
+    }',
   PRIMARY KEY (id),
   FOREIGN KEY (player1ID) REFERENCES users(id),
-  FOREIGN KEY (player2ID) REFERENCES users(id)
+  FOREIGN KEY (player2ID) REFERENCES users(id),
 );
 
 
 
 INSERT into users (username, password, wins, losses, playerLevel, games, avatarUrl)
-Values ('boxingBakerBen', 'password1', 6, 4, 2, '[1]', 'http://oi46.tinypic.com/4jl55k.jpg');
+Values ('boxingBakerBen', 'password1', 6, 4, 2, '[1]', '[]', 'http://oi46.tinypic.com/4jl55k.jpg');
 
 INSERT into users (username, password, wins, losses, playerLevel, games, avatarUrl)
-Values ('doughDudeDevon', 'password2', 3, 7, 1, '[1]', 'http://oi48.tinypic.com/2n9z89k.jpg');
+Values ('doughDudeDevon', 'password2', 3, 7, 1, '[1]', '[]', 'http://oi48.tinypic.com/2n9z89k.jpg');
 
 INSERT into users (username, password, wins, losses, playerLevel, games, avatarUrl)
-Values ('empanadaEaterEphraim', 'password3', 5, 5, 2, '[2]', 'http://oi50.tinypic.com/241jksn.jpg');
+Values ('empanadaEaterEphraim', 'password3', 5, 5, 2, '[2]', '[]', 'http://oi50.tinypic.com/241jksn.jpg');
 
 INSERT into users (username, password, wins, losses, playerLevel, games, avatarUrl)
-Values ('sniperSouffleSteph', 'password4', 8, 2, 3, '[2]', 'http://oi43.tinypic.com/5twb9d.jpg');
+Values ('sniperSouffleSteph', 'password4', 8, 2, 3, '[2]', '[]', 'http://oi43.tinypic.com/5twb9d.jpg');
 
 
 INSERT into games (player1ID, player1Placement, player1Hits, player1Misses, player2ID, 
